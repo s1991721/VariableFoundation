b472758b02785fb3e3feb319c883a64a7e4eccf2
mr.lin <JLin@kmhealthcloud.com>
2018-11-09 17:30:16 +0800

    ImageLoader第一版完成

--- app/src/main/java/com/ljf/variablefoundation/MainActivity.java
+++ app/src/main/java/com/ljf/variablefoundation/MainActivity.java
@@ -4,29 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.ljf.variablefoundation.http.OnResponseListener;
+import com.ljf.variablefoundation.image.ImageLoader;
 
 /**
  * Created by mr.lin on 2018/11/7
  */
 public class MainActivity extends Activity {
-    Button button;
+    Button httpBt;
+    Button imageBt;
+    ImageView imageV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        httpBt = findViewById(R.id.httpBt);
+        httpBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new LoginRequester(new OnResponseListener<LoginInfo>() {
                     @Override
                     public void onResponse(int code, LoginInfo data) {
-                        button.setText(data.getName() + "\n" + data.getPwd());
+                        httpBt.setText(data.getName() + "\n" + data.getPwd());
                     }
                 }).request();
+            }
+        });
+        imageBt = findViewById(R.id.imageBt);
+        imageV = findViewById(R.id.imageV);
+        imageBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageLoader.load(imageV, "https://www.baidu.com/img/bd_logo2.png").setErrorResId(R.mipmap.ic_launcher).setCircle(true).apply();
             }
         });
     }
---
+++ app/src/main/java/com/ljf/variablefoundation/image/IImageLoader.java
@@ -0,0 +1,11 @@
+package com.ljf.variablefoundation.image;
+
+/**
+ * Created by mr.lin on 2018/11/9
+ * 通用图片加载接口
+ */
+public interface IImageLoader {
+
+    void apply(ImageLoader.Builder builder);
+
+}
