195a6c518c107b1213653725b9a4c2fce3721ce5
s1991721 <278971585@qq.com>
2018-11-16 17:20:58 +0800

    UserManager回调

--- app/src/main/java/com/ljf/variablefoundation/UserManager.java
+++ app/src/main/java/com/ljf/variablefoundation/UserManager.java
@@ -2,10 +2,15 @@ package com.ljf.variablefoundation;
 
 import com.ljf.variablefoundation.base.BaseManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mr.lin on 2018/11/13
  */
 public class UserManager extends BaseManager {
+
+    private List<OnUserStateChangeListener> onUserStateChangeListeners = new ArrayList<>();
 
     @Override
     protected void onManagerCreate() {
@@ -22,6 +27,33 @@ public class UserManager extends BaseManager {
 
     public String getUID() {
         return "";
+    }
+
+    public boolean isLogedIn() {
+        return true;
+    }
+
+    //Observer
+
+    public interface OnUserStateChangeListener {
+        void onStateChange(int state);
+    }
+
+    public static final int STATE_ONLINE = 0;
+    public static final int STATE_OFFLINE = 1;
+
+    public void addOnUserStateChangeListener(OnUserStateChangeListener onUserStateChangeListener) {
+        onUserStateChangeListeners.add(onUserStateChangeListener);
+    }
+
+    public void removeOnUserStateChangeListener(OnUserStateChangeListener onUserStateChangeListener) {
+        onUserStateChangeListeners.remove(onUserStateChangeListener);
+    }
+
+    private void notifyUserStateChange(int state) {
+        for (OnUserStateChangeListener listener : onUserStateChangeListeners) {
+            listener.onStateChange(state);
+        }
     }
 
 }
