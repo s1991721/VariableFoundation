01db1f7a4f8251cf0765b0b53e8cf23e90f251cb
s1991721 <278971585@qq.com>
2019-01-24 11:07:41 +0800

    更新lib

---
+++ app/src/main/java/com/ljf/variablefoundation/VFApplication.java
@@ -0,0 +1,36 @@
+package com.ljf.variablefoundation;
+
+import com.canceraide.mylibrary.base.BaseApplication;
+import com.canceraide.mylibrary.base.BaseManager;
+
+import java.util.List;
+
+/**
+ * Created by mr.lin on 2019/1/24
+ */
+public class VFApplication extends BaseApplication {
+    @Override
+    protected void registerManager(List<BaseManager> managers) {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public void triggerLogin() {
+
+    }
+
+    @Override
+    public void triggerHome() {
+
+    }
+
+    @Override
+    public void triggerComplete() {
+
+    }
+}
